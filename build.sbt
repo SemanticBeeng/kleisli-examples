40f0d90a657a4bdb2284d6f86d43c30ac0fab9cb
Semantic Beeng <nick@me.com>
2016-05-12 16:05:29 +0000

    added example from http://debasishg.blogspot.com/2013/02/a-dsl-with-endo-monoids-for-free.html

--- build.sbt
+++ build.sbt
@@ -4,9 +4,12 @@ val kleisli: Project = (project in file(".")).
                        settings(commonSettings: _*).
                        settings(addCompilerPlugin("org.spire-math" %% "kind-projector" % "0.6.3")).
                        settings(
-                           name := "kleisli"
+                           name := "kleisli",
+                             libraryDependencies ++= Seq(
+                             "org.scalaz" %% "scalaz-core" % "7.2.2"
+                             )
                          )
 
 
 
-    
+
---
+++ src/main/scala/org/debasishg/snippet/endodsl.scala
@@ -0,0 +1,52 @@
+package org.debasishg.snippet
+
+import java.util.{ Date, Calendar }
+import scalaz._
+import Endo._
+import WriterT._
+import scalaz.syntax.all._
+import scalaz.syntax.std.all._
+
+object endodsl {
+  case class Task(name: String) {
+    def dependsOn(on: Task): Writer[Endo[Project], Task] = {
+      for {
+        _ <- tell(((p: Project) => withDependency(this, on, p)).endo)
+      } yield this
+    }
+  }
+
+  case class Project(name: String, startDate: java.util.Date, endDate: Option[java.util.Date] = None,
+                     tasks: List[Task] = List(), deps: List[(Task, Task)] = List())
+
+  val withTask = (t: Task, p: Project) => p.copy(tasks = t :: p.tasks)
+  val withDependency = (t: Task, on: Task, p: Project) => p.copy(deps = (t, on) :: p.deps)
+
+  def task(n: String): Writer[Endo[Project], Task] = {
+    val t = Task(n)
+    for {
+      _ <- tell(((p: Project) => withTask(t, p)).endo)
+    } yield t
+  }
+
+  def project(name: String, startDate: Date)(e: Writer[Endo[Project], Task]) = {
+    val p = Project(name, startDate)
+    e.run._1(p)
+  }
+}
+
+object Main {
+  import org.debasishg.snippet.endodsl._
+  val now = Calendar.getInstance.getTime
+  val p = project("xenos", now) {
+      for {
+        a <- task("study requirements")
+        b <- task("do analysis")
+        _ <- b dependsOn a
+        c <- task("design & code")
+        _ <- c dependsOn b
+        d <- c dependsOn a
+      } yield d
+    }
+  println(p)
+}
